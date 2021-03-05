383b063c5497c8d3b824c5da81e874fda0029584
Juliana <julianadecarvalho92@gmail.com>
2021-03-05 12:09:21 -0500

    Added Service and Broadcast
    Added Service and Broadcast

--- app/src/main/java/com/JDC/bot/ChatBotService.java
+++ app/src/main/java/com/JDC/bot/ChatBotService.java
@@ -2,9 +2,19 @@ package com.JDC.bot;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 public class ChatBotService extends Service {
+
+    private static final String TAG = "ChatBotService";
+
+    public static final String CMD = "cmd";
+    public static final int CMD_GENERATE_MSG = 1;
+    public static final int CMD_STOP_SERVICE = 2;
+    public static final String MESSAGE_TEXT = "message";
+
     public ChatBotService() {
     }
 
@@ -12,5 +22,43 @@ public class ChatBotService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v(TAG, "onStartCommand()");
+        super.onStartCommand(intent, flags, startId);
+        if (intent != null) {
+            Bundle data = intent.getExtras();
+
+            int command = data.getInt(CMD);
+            Log.d(TAG, "-(<- received command data to service: command=" + command);
+
+            if (command == CMD_GENERATE_MSG) {
+
+                String responseMessage = data.getString(MESSAGE_TEXT);
+                sendBroadcastConnected(responseMessage);
+
+            } else if (command == CMD_STOP_SERVICE) {
+
+            } else {
+                Log.w(TAG, "Ignoring Unknown Command! id=" + command);
+            }
+        }
+        return START_STICKY;
+    }
+
+    private void sendBroadcastConnected(String responseMessage) {
+        Log.d(TAG, "->(+)<- sending broadcast: BROADCAST_SERVER_CONNECTED");
+        Intent responseIntent = new Intent();
+
+        Bundle responseData = new Bundle();
+        responseData.putString("value",responseMessage);
+
+        responseIntent.putExtras(responseData);
+
+        responseIntent.setAction("SERVICE_RESPONSE_GENERATE_MESSAGE");
+
+        sendBroadcast(responseIntent);
     }
 }
--- app/src/main/java/com/JDC/bot/MainActivity.java
+++ app/src/main/java/com/JDC/bot/MainActivity.java
@@ -2,13 +2,68 @@ package com.JDC.bot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("SERVICE_RESPONSE_GENERATE_MESSAGE");
+        registerReceiver(chatBotServiceResponseReceiver, intentFilter);
+
+        Button btnGenerateMsg = (Button) findViewById(R.id.btnGenerateMsg);
+        btnGenerateMsg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Getting a reference
+                messages = findViewById(R.id.txtMessages);
+
+                Bundle data = new Bundle();
+                data.putInt(ChatBotService.CMD, ChatBotService.CMD_GENERATE_MSG);
+                data.putString(ChatBotService.MESSAGE_TEXT, "Hello!");
+                Intent intent = new Intent(getApplicationContext(), ChatBotService.class);
+                intent.putExtras(data);
+                startService(intent);
+
+            }
+        });
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(chatBotServiceResponseReceiver);
+    }
+
+    private final BroadcastReceiver chatBotServiceResponseReceiver = new BroadcastReceiver() {
+        private static final String TAG = "BroadcastReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Bundle data = intent.getExtras();
+            String valueString = data.getString("value");
+
+            Log.d(TAG, "received broadcast message from service: " + action);
+
+            messages.append(valueString+ "\n");
+
+        }
+
+    };
 }
